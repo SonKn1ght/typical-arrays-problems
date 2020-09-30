331f74c67c4b478ade18c740702557d8855ef959
SonKn1ght <maximiilian321@gmail.com>
2020-09-30 15:47:29 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if (array.length == 0) {
+        return 0
+    }
+    let a = array.sort((a, b) => {
+        return a - b;
+    })[0]
+    return a;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    if (array.length == 0) {
+        return 0
+    }
+    let a = array.sort((a, b) => {
+        return b - a;
+    })[0]
+    return a;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    if (array.length == 0) {
+        return 0
+    }
+    let a = array.reduce((acc, cur) => {
+        return acc + cur;
+    }, 0)/(array.length)
+  return a;
 }
